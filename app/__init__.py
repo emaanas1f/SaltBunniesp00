e11070751c2afd3f86d489f5b7a3e6ea0e2210c4
James Lei <leijames325@gmail.com>
2025-10-28 16:55:12 -0400

    Restructured app

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,10 @@
-from flask import Flask, render_template, request
-from db import select_query, insert_query
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    blogs = select_query('SELECT blog_name FROM blogs')
-    return render_template('home.html', blog)
+  return render_template('home.html')
 
-@app.route('/profile', methods=['GET'])
-def get_profile():
-    username = request.args['username']
-    blogs = select_query("SELECT blog_name FROM blogs WHERE user='?'", [username])
-    return render_template('profile.html', blogs)
-
-@app.route('/profile', methods=['POST'])
-def post_profile():
-    blog_name = request.form['name']
-    content = request.form['content']
-    if len(select_query('SELECT * FROM blogs WHERE blog_name=?', [blog_name])) != 0:
-        flash("Blog with that name already exists!")
-        return redirect('/profile')
-    inset_query("INSERT into REQUESTS ()
-
-@app.route('/blog', methods=['GET'])
-def blog():
-    blog_name = request.args['name']
-    entries = select_query("SELECT id,content FROM entries WHERE blog='?' SORT BY date_created", [blog_name])
-    return render_template('blog.html', entries)
-
-@app.route('/post', methods=['GET'])
-def post():
-    post_id = request.args[]
-    return render_template('post.html')
-
-
-@app.route('/edit', methods=['POST'])
-def edit():
-    if (request.args['type'] == 'create_blog'):
-        
-        
-            
+if __name__ == "__main__":
+    app.run()
