da9577d400bb78c74a94ec8e3d167a4f8b8209c8
Kiran <kirans14@nycstudents.net>
2025-10-29 11:00:08 -0400

    finish select_query method

--- app/build_db.py
+++ app/build_db.py
@@ -6,18 +6,19 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
 
-c.execute("DROP TABLE IF EXISTS profile") # check for already existing entries table
+c.execute("DROP TABLE IF EXISTS profiles") # check for already existing entries table
 c.execute("CREATE TABLE profiles (username TEXT PRIMARY KEY, password TEXT);")
 
 c.execute("DROP TABLE IF EXISTS blogs")
-c.execute("CREATE TABLE blogs (blog_id INTEGER PRIMARY KEY, blog_name TEXT, user TEXT, date_created DATETIME, FOREIGN KEY (user) REFERENCES profile(username));")
+c.execute("CREATE TABLE blogs (id INTEGER PRIMARY KEY, title TEXT, user TEXT, date_created DATETIME, FOREIGN KEY (user) REFERENCES profile(username));")
 
 c.execute("DROP TABLE IF EXISTS entries") 
-c.execute("CREATE TABLE entries (blog INTEGER, id INTEGER PRIMARY KEY, date_created DATETIME, content TEXT, recent_edit DATETIME, FOREIGN KEY (blog) REFERENCES blogs(blog_id));")
+c.execute("CREATE TABLE entries (blog INTEGER, id INTEGER PRIMARY KEY, date_created DATETIME, content TEXT, recent_edit DATETIME, FOREIGN KEY (blog) REFERENCES blogs(id));")
 
 c.execute("DROP TABLE IF EXISTS edits")
 c.execute("CREATE TABLE edits (entry_id INTEGER, user TEXT, timestamp DATETIME, updated_content TEXT, FOREIGN KEY (entry_id) REFERENCES entries(id), FOREIGN KEY (user) REFERENCES profile(username));")
 
-
+c.execute("INSERT INTO profiles VALUES ('U1', 'pwrd1');")
+c.execute("INSERT INTO profiles VALUES ('U2', 'pwrd2');")
 db.commit()
 db.close()
--- app/db.py
+++ app/db.py
@@ -5,7 +5,20 @@ DB_FILE="data.db"
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
-def select_query(query_string, parameters):
-    outstring = query_string
-    c.execute(query_string, (parameters)) #FINISH
-    return c.fetchall()
+def select_query(query_string, parameters=()):
+    c.execute(query_string, parameters)
+    out_array = []
+    column_names = c.description
+    for row in c.fetchall():
+        item_dict = dict()
+        for col in range(len(row)):
+             item_dict.update({column_names[col][0]: row[col]})
+        out_array.append(item_dict)
+    return out_array
+
+print(select_query("SELECT * FROM profiles WHERE username = ? ", ("U1",))) 
+
+# general query function
+# insert-query function
+db.commit()
+db.close()
