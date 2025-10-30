5f880753dcfe3bb69a412422b29994365ef2e704
emaan asif <emaanasif438@gmail.com>
2025-10-29 22:18:00 -0400

    auth.py + devlog

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/login', methods=['GET'])
-def get_login():
-  return
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username:
+            session['username'] = username
+            return redirect(url_for('auth.home'))
+        else:
+            return render_template('login.html', error='Please enter a username')
+
+    return render_template('login.html')
+
+@bp.route('/home')
+def home():
+    if 'username' not in session:
+        return redirect(url_for('auth.login'))
+    return render_template('home.html', username=session['username'])
+
+@bp.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('auth.login'))
+
   # BLAH BLAH BLAH
 
 # @app.route("/login", methods=['GET', 'POST'])
