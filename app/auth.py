f0feacf2e79f3c7a4684a56a5f919559eb448cfe
emaan asif <emaanasif438@gmail.com>
2025-10-29 22:20:09 -0400

    removing old work

--- app/auth.py
+++ app/auth.py
@@ -20,48 +20,10 @@ def home():
         return redirect(url_for('auth.login'))
     return render_template('home.html', username=session['username'])
 
+#logout 
 @bp.route('/logout')
-def logout():
-    session.pop('username', None)
+   def logout():
+   session.pop('username', None)
     return redirect(url_for('auth.login'))
 
-  # BLAH BLAH BLAH
 
-# @app.route("/login", methods=['GET', 'POST'])
-# def disp_loginpage():
-#     print("\n\n\n")
-#     print("***DIAG: this Flask obj ***")
-#     print(app)
-#     print("***DIAG: request obj ***")
-#     print(request)
-#     print("***DIAG: request.args ***")
-#     if(request.method == 'GET'):
-#         print(request.args)
-#         print("***DIAG: request.headers ***")
-#         print(request.headers)
-#         if 'username' in session:
-#             return render_template('home.html', username = session['username'])
-#         else:
-#             return render_template('login.html')
-
-
-# @app.route("/auth")
-# def authenticate():
-#     print("\n\n\n")
-#     print("***DIAG: this Flask obj ***")
-#     print(app)
-#     print("***DIAG: request obj ***")
-#     print(request)
-#     print(request.args)
-#     print("***DIAG: request.args['username']  ***")
-#     if(request.method == 'POST'):
-#         username = request.form.get('username')
-#     else:
-#         username = request.args.get('username')
-#     if username:
-#         session['username'] = username
-#         return render_template('home.html', username=username)
-#     else:
-#         return redirect(url_for('disp_loginpage'))
-#     print("***DIAG: request.headers ***")
-#     print(request.headers)
