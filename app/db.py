2fc6a88dbdfa0b9249c239e83d9005825fe742fc
Kiran <kirans14@nycstudents.net>
2025-10-30 10:55:33 -0400

    fix f-string issues and closing cursor

--- app/db.py
+++ app/db.py
@@ -3,9 +3,9 @@ import sqlite3
 DB_FILE="data.db"
 
 db = sqlite3.connect(DB_FILE)
-c = db.cursor()
 
 def select_query(query_string, parameters=()):
+    c = db.cursor()
     c.execute(query_string, parameters)
     out_array = []
     column_names = c.description
@@ -14,22 +14,27 @@ def select_query(query_string, parameters=()):
         for col in range(len(row)):
              item_dict.update({column_names[col][0]: row[col]})
         out_array.append(item_dict)
+    c.close()
     return out_array
 
 def insert_query(table, data):
-    insertable_data = []
-    for item in data.values():
-        if isinstance(item, str):
-            insertable_data.append("'" + item + "'")
-        else:
-            insertable_data.append(item)
-    print(insertable_data)
-    c.execute(f"INSERT INTO {table} VALUES ({", ".join(insertable_data)});")
+    c = db.cursor()
+#    insertable_data = []
+#    for item in data.values():
+#        if isinstance(item, str):
+#            insertable_data.append("'" + item + "'")
+#        else:
+#            insertable_data.append(item)
+    # print(insertable_data)
+    print(data.values())
+    placeholder = ["?"] * len(data)
+    c.execute(f"INSERT INTO {table} VALUES ({', '.join(placeholder)});", tuple(data.values()))
+    c.close()
+    db.commit()
 
-# print(select_query("SELECT * FROM profiles WHERE username = ? ", ("U1",))) 
-# insert_query("profiles", {"username":"U3", "password":"pswd"})
-# print(select_query("SELECT * FROM profiles"))
+#print(select_query("SELECT * FROM profiles WHERE username = ? ", ("U1",))) 
+#insert_query("profiles", {"username":"U3", "password":"pswd"})
+#print(select_query("SELECT * FROM profiles"))
 
-# general query function
-db.commit()
-db.close()
+
+
