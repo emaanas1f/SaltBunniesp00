83b64c191a0f86427c2c7f77181fbb1ad503d043
@Kiran1029 <kiranjsoemardjo@gmail.com>
2025-11-03 17:50:59 -0500

    add general_query method and fix insert_query output content

--- app/db.py
+++ app/db.py
@@ -21,18 +21,26 @@ def select_query(query_string, parameters=()):
 def insert_query(table, data):
     c = db.cursor()
     placeholder = ["?"] * len(data)
-    c.execute(f"INSERT INTO {table} {tuple(data.keys())} VALUES ({', '.join(placeholder)}) RETURNING rowid;", tuple(data.values()))
-    row = c.fetchall()[0][0]
+    c.execute(f"INSERT INTO {table} {tuple(data.keys())} VALUES ({', '.join(placeholder)}) RETURNING *;", tuple(data.values()))
+    row = c.fetchall()
+    output = dict()
+    for col in range(len(row[0])):
+        output.update({c.description[col][0]: row[0][col]}) 
     c.close()
     db.commit()
-    output = dict(row = row)
     return output
 
+def general_query(query_string):
+    c = db.cursor()
+    c.execute(query_string)
+    c.close()
+    db.commit()
+    
 #print(select_query("SELECT * FROM profiles WHERE username = ? ", ("U1",))) 
 #print(insert_query("profiles", {"username":"U3", "password":"pswd"}))
 #print(select_query("SELECT * FROM profiles"))
-#
 #print(insert_query("blogs", {"title": "Bg3", "user": "U3"}))
+#general_query("INSERT INTO blogs (title, user) VALUES ('Bg3', 'U3')")
 #print(select_query("SELECT * FROM blogs"))
 
 
