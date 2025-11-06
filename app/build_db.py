1d0a9bae0ff96c9b7852f9f29abe14a8f67c0a4f
Kiran <kirans14@nycstudents.net>
2025-11-06 10:33:04 -0500

    clean up data initialization code

--- app/build_db.py
+++ app/build_db.py
@@ -6,17 +6,54 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
 c.executescript("""
-    DROP TABLE IF EXISTS profiles;
-    CREATE TABLE profiles (username TEXT PRIMARY KEY, password TEXT);
-    DROP TABLE IF EXISTS blogs;
-    CREATE TABLE blogs (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, user TEXT, date_created DATETIME DEFAULT CURRENT_TIMESTAMP, follows INTEGER DEFAULT 0, views INTEGER DEFAULT 0, FOREIGN KEY (user) REFERENCES profiles(username));
-    DROP TABLE IF EXISTS entries;
-    CREATE TABLE entries (blog INTEGER, id INTEGER PRIMARY KEY AUTOINCREMENT, date_created DATETIME DEFAULT CURRENT_TIMESTAMP, content TEXT, recent_edit DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (blog) REFERENCES blogs(id));
-    DROP TABLE IF EXISTS edits;
-    CREATE TABLE edits (entry INTEGER, user TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, updated_content TEXT, FOREIGN KEY (entry) REFERENCES entries(id), FOREIGN KEY (user) REFERENCES profiles(username));
-    DROP TABLE IF EXISTS follows;
-    CREATE TABLE follows (user TEXT, blog INTEGER, FOREIGN KEY (user) REFERENCES profiles(username), FOREIGN KEY (blog) REFERENCES blogs(id));
-    """)
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+    username TEXT PRIMARY KEY, 
+    password TEXT
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS blogs;
+CREATE TABLE blogs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    title TEXT UNIQUE, 
+    user TEXT, 
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    follows INTEGER DEFAULT 0, 
+    views INTEGER DEFAULT 0, 
+    FOREIGN KEY (user) REFERENCES profiles(username)
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS entries;
+CREATE TABLE entries (
+    blog INTEGER, 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    content TEXT, 
+    recent_edit DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (blog) REFERENCES blogs(id)
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS edits;
+CREATE TABLE edits (
+    entry INTEGER, 
+    user TEXT, 
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    updated_content TEXT, 
+    FOREIGN KEY (entry) REFERENCES entries(id), 
+    FOREIGN KEY (user) REFERENCES profiles(username)
+);""")
+
+c.executescript("""
+DROP TABLE IF EXISTS follows;
+CREATE TABLE follows (
+    user TEXT, 
+    blog INTEGER, 
+    FOREIGN KEY (user) REFERENCES profiles(username), 
+    FOREIGN KEY (blog) REFERENCES blogs(id)
+);""")
     
 
 db.commit()
